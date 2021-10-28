c15baf610c239e9f3fe72db822dd0d6687fd05ed
Didi Prasetyo <didiprasetyo360@gmail.com>
2021-10-28 17:08:54 +0700

    mengubah tampilan landing page dengan menambahkan pricing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,8 @@
 
     @include('layouts.nav')
     
-    <div class="main">
+    {{-- beranda --}}
+    <div class="beranda">
       <div class="hero-image h-screen relative">
         <div class="flex absolute top-20 left-20 w-1/2 h-72 bg-blue-200 rounded-md p-2 py-8">
           <h1 class="w-2/5 text-4xl text-blue-800">Buat Undangan Pernikahan Online Gratis Kamu Disini!</h1>
@@ -24,6 +25,68 @@
         </div>
       </div>
     </div>
+
+    {{-- paket pembelian --}}
+    <div class="flex min-h-screen justify-around mb-14">
+      <div class="w-3/12 h-96 p-7 shadow-lg rounded-md border-t-2 hover:shadow-2xl hover:cursor-pointer">
+        <h1 class="text-4xl">Bronze</h1>
+        <p>paket bronze</p>
+
+        <div class="mt-7 mb-5">
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <p>Lorem ipsum dolor sit.</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+
+        <a href="#" class="inline-block px-5 py-2 rounded-md border-2 text-blue-300 bg-white border-blue-300 hover:bg-blue-300 hover:text-white">Pilih Paket</a>
+      </div>
+
+      <div class="w-3/12 h-1/2 p-7 shadow-lg rounded-md border-t-2 hover:shadow-2xl hover:cursor-pointer">
+        <h1 class="text-4xl">Silvel</h1>
+        <p>paket silvel</p>
+
+        <div class="mt-7 mb-5">
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <p>Lorem ipsum dolor sit.</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <p>Lorem ipsum dolor sit.</p>
+        </div>
+
+        <a href="#" class="inline-block px-5 py-2 rounded-md border-2 text-blue-300 bg-white border-blue-300 hover:bg-blue-300 hover:text-white">Pilih Paket</a>
+      </div>
+
+      <div class="w-3/12 p-7 shadow-lg rounded-md border-t-2 hover:shadow-2xl hover:cursor-pointer">
+        <h1 class="text-4xl">Gold</h1>
+        <p>paket gold</p>
+
+        <div class="mt-7 mb-5">
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <p>Lorem ipsum dolor sit.</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <p>Lorem ipsum dolor sit.</p>
+          <p>Lorem ipsum dolor sit.</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <p>Lorem ipsum dolor sit.</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+
+        <a href="#" class="inline-block px-5 py-2 rounded-md border-2 hover:text-blue-300 hover:bg-white border-blue-300 bg-blue-300 text-white">Pilih Paket</a>
+      </div>
+      
+    </div>
+
     <footer class="w-full h-28 bg-gray-600 flex items-center">
       <h1 class="mx-auto text-white">Copyright undangan-Online.id</h1>
     </footer>
